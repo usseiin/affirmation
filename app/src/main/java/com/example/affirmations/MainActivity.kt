75875eb6d580c5c8197d4b3f5e2fd61fa4b6d4ee
usseiin <sirkenny.hk@gmail.com>
2023-01-22 17:00:50 +0100

    initial commit

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -17,8 +17,29 @@ package com.example.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,5 +54,54 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AffirmationApp() {
     AffirmationsTheme {
+        AffirmationList(
+            affirmationList = Datasource().loadAffirmations()
+        )
+    }
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(
+        modifier = Modifier.padding(10.dp),
+        elevation = 4.dp
+    ) {
+        Column() {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = affirmation.stringResourceId.toString(),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp)
+            )
+            Text(
+                stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .padding(16.dp),
+                style = MaterialTheme.typography.h6
+            )
+        }
+    }
+}
+
+@Composable
+private fun AffirmationList(affirmationList: List<Affirmation>) {
+    LazyColumn() {
+        items(affirmationList) {
+            affirmation -> AffirmationCard(
+                affirmation = affirmation
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AffirmationPreview() {
+    AffirmationsTheme {
+        AffirmationList(
+            affirmationList = Datasource().loadAffirmations()
+        )
     }
 }
--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -15,12 +15,13 @@
  */
 package com.example.affirmations.data
 import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
 
 /**
  * [Datasource] generates a list of [Affirmation]
  */
 class Datasource() {
-    /*fun loadAffirmations(): List<Affirmation> {
+    fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2),
@@ -31,6 +32,7 @@ class Datasource() {
             Affirmation(R.string.affirmation7, R.drawable.image7),
             Affirmation(R.string.affirmation8, R.drawable.image8),
             Affirmation(R.string.affirmation9, R.drawable.image9),
-            Affirmation(R.string.affirmation10, R.drawable.image10))
-    }*/
+            Affirmation(R.string.affirmation10, R.drawable.image10)
+        )
+    }
 }
